845caa8f69d675aadee7fc4b1498e9c547692123
wojto <wojtowiczp97@gmail.com>
2023-08-05 23:39:53 +0200

    create kedro-project

---
+++ kedro-project/src/kedro_project/constants.py
@@ -0,0 +1,6 @@
+BASE_DATA_DIR = 'data/01_raw'
+UNWANTED_COLUMN_NAMES = ['id']
+RESULTS_COLUMN_NAME = 'stroke'
+CATEGORICAL_COLUMN_NAMES = ['gender', 'ever_married', 'work_type', 'Residence_type', 'smoking_status']
+COLUMN_MAPPING_DICTIONARY = 'mapping'
+FINAL_DATA_FILENAME = 'data/05_model_input/final_data.csv'
---
+++ test.py
@@ -0,0 +1,3 @@
+import wandb
+
+wandb.login(key="PASS_WANDB_KEY_HERE")
